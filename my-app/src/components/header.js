1aff8f55b2cd9aa3df5b4988e6a115bc44343bb6
Jennifer Zhang <jlz2@andrew.cmu.edu>
2024-01-31 20:07:57 -0800

    changed header

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,58 +1,25 @@
-import React, { useState,useEffect } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-import {TitleAndIcon } from '../components/titleAndIcon';
-
-
-const Navbar = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: black;
-  padding: 20px;
-  // z-index: 0;
-`;
-
-
-const NavLink = styled(Link)`
-  color: #AAAAAA;
-  text-decoration: none;
-  font-weight: 700;
-  position: relative;
-  padding: 8px 16px;
-  transition: all 0.3s ease-in-out;
-  margin: 20px;
-
-  &::before {
-    content: "";
-    background-color: hsla(196, 61%, 58%, 0.5);
-    position: absolute;
-    left: 0;
-    bottom: 1px;
-    width: 100%;
-    height: 4px;
-    z-index: 1;
-    transition: all 0.3s ease-in-out;
-  }
-
-  &:hover::before {
-    bottom: 0;
-    height: 100%;
-    border-radius: 20px;
-  }
-  &:hover {
-    color: white;
-  }
-`;
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { TitleAndIcon } from "../components/titleAndIcon";
 
 const Header = () => {
   return (
-    <Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg" className="px-5 py-2">
+      <Navbar.Brand>
         <TitleAndIcon />
-      <div>
-        <NavLink to="/about">Resume</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-      </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="">
+          <LinkContainer to="/about">
+            <Nav.Link>Resume</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/projects">
+            <Nav.Link>Projects</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,68 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
-import { DotLottiePlayer } from '@dotlottie/react-player';
-
-const Hero = styled.div`
-    box-sizing: border-box;
-    flex-shrink: 0;
-    width: 100%;
-    height: 100vh; /* 776px */
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 0px 100px 100px 100px;
-    background-color: var(--dark_blue, #153243);
-    min-height: 100vh;
-    overflow: hidden;
-    z-index: 1;
-    position: relative;
-    align-content: center;
-    flex-wrap: nowrap;
-    gap: 0;
-    border-radius: 0px 0px 0px 0px;
-`;
-
-const Name = styled.h1`
-flex-shrink: 0;
-width: 1000px;
-height: auto; /* 97px */
-white-space: pre;
-position: relative;
-font-weight: 700;
-font-style: normal;
-font-family: "Unbounded", sans-serif;
-color: var(--lighter_blue, #284b63);
-// font-size: 99.45449836420218px;
-letter-spacing: 0em;
-text-transform: uppercase;
-line-height: 1.1;
-text-align: center;
-`
+import React from "react";
+import { Container } from "react-bootstrap";
+import { DotLottiePlayer } from "@dotlottie/react-player";
+// import "./customStyles.css"; // For any custom styles not covered by Bootstrap
 
 const Home = () => {
-    return (
-      <Hero>
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%) rotate(90deg)",
-            width: "100vh", // Using vh to match the viewport height
-            height: "100vw", // Using vw to match the viewport width
-          }}
-        >
-          <DotLottiePlayer
-            src="https://lottie.host/1d2ac87f-fa4a-4fd9-89b0-624da7c26017/7gDd1MBonF.lottie"
-            autoplay
-            loop
-            style={{ width: "100%", height: "100%" }}
-          />
-        </div>
-        <Name style={{ position: "relative", zIndex: 1 }}>Jennifer Zhang</Name>
-      </Hero>
-    );
+  return (
+    <Container
+      fluid
+      className="d-flex flex-column justify-content-center align-items-center p-4 bg-dark text-center"
+      style={{
+        minHeight: "100vh",
+        position: "relative",
+        overflow: "hidden",
+        zIndex: 1,
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%) rotate(90deg)",
+          width: "100vh",
+          height: "100vw",
+        }}
+      >
+        <DotLottiePlayer
+          src="https://lottie.host/1d2ac87f-fa4a-4fd9-89b0-624da7c26017/7gDd1MBonF.lottie"
+          autoplay
+          loop
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+      <h1 className="fw-bold custom-name">Jennifer Zhang</h1>{" "}
+      {/* For custom styles use a separate class */}
+    </Container>
+  );
 };
 
 export default Home;
