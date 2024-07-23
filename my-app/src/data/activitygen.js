0d2d93f9f7121d858580f37a7a2fa05e82efd569
Jennifer Zhang <jlz2@andrew.cmu.edu>
2024-07-23 16:58:49 -0700

    added activity generator source code url

--- my-app/src/data/activitygen.js
+++ my-app/src/data/activitygen.js
@@ -79,6 +79,8 @@ export const activitygen = {
   category: ["code"],
   active: true,
   course: "67-328 Full Stack Application and Development",
-  description: "The \"Activity Generator\" is a web app I developed to suggest personalized activities to users based on their preferences. Using HTML, CSS, JavaScript, and jQuery, I built this full-stack project in 5 months, enhancing my skills in web development.",
+  description:
+    'The "Activity Generator" is a web app I developed to suggest personalized activities to users based on their preferences. Using HTML, CSS, JavaScript, and jQuery, I built this full-stack project in 5 months, enhancing my skills in web development.',
   details: activitygenDetails,
+  sourceCodeUrl: "https://main--phenomenal-meerkat-481ce0.netlify.app/",
 };
